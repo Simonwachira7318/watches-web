bd830a64e707af129b3be755df06d4a12bcd9ae0
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-07-11 11:05:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// redirection of social media accounts
+function redirectToFacebook() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100071584461977";
+  }
+  // redirection of social media accounts
+function redirectToInstagram() {
+    window.location.href = "https://instagram.com/its__wachira?igshid=NGExMmI2YTkyZg==";
+  }
+  
+  // redirection of social media accounts
+  function redirectToTwitter() {
+    window.location.href = "https://twitter.com/Its_wachira2?t=P5t0BhEmEFgfYGYECE7OdQ&s=35";
+  }
+  
+  // redirection of social media accounts
+  function redirectToLinkedin() {
+    window.location.href = "https://www.linkedin.com/in/simon-wachira-680b88268";
+  }
+  
